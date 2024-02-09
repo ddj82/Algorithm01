fb2b877f905515ff2e4113e44133ef73360401af
ddj82 <145192579+ddj82@users.noreply.github.com>
2024-02-10 00:51:06 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.03 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,15 +1,8 @@
-import java.util.*;
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        ArrayList<Integer> a = new ArrayList<Integer>();
-        for (int i = 0; i < num_list.length; i++) {
-            if (i >= n - 1) {
-                a.add(num_list[i]);
-            }
-        }
-        int[] answer = new int[a.size()];
+        int[] answer = new int[num_list.length - (n - 1)];
         for (int i = 0; i < answer.length; i++) {
-            answer[i] = a.get(i);
+            answer[i] = num_list[(n - 1) + i];
         }
         return answer;
     }
