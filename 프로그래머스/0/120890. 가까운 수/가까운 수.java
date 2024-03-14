abb38e2bf9768ed83c27aa5c745661e2972c1a8a
ddj82 <145192579+ddj82@users.noreply.github.com>
2024-03-14 18:55:35 +0900

    [level 0] Title: 가까운 수, Time: 0.10 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (Math.abs(n - answer) > Math.abs(n - array[i])) {
+                answer = array[i];
+            } else if (Math.abs(n - answer) == Math.abs(n - array[i])) {
+                if (answer > array[i]) {
+                    answer = array[i];
+                }
+            }
+        }
+        return answer;
+    }
+}
